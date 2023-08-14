649d2afd0a4aca6b88c90e087763b2ceceae158d
PNelligan00 <79494002+PNelligan00@users.noreply.github.com>
2023-08-14 11:13:24 -0700

    Update mod.js
    Just getting started

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Manufactor-Tree",
+	id: "Factory_Mod",
+	author: "nobody1231",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
